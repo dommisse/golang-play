ef2fcc2656c1ee216f91e0d180c5326399e63b1b
david dommisse <david.dommisse@allangray.co.za>
2021-11-14 10:33:41 +0200

    feature: function type

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"dj.com/functions"
 	_ "dj.com/functions"
 	"fmt"
 	"math"
@@ -9,9 +10,10 @@ import (
 func main() {
 	op := math.Abs
 	a := []float64{1, -2}
-	b := Map(op, a)
+
+	b := functions.Map(op, a)
 	fmt.Println(b) // [1 2]
 
-	c := Map(func(x float64) float64 { return 10 * x }, b)
+	c := functions.Map(func(x float64) float64 { return 10 * x }, b)
 	fmt.Println(c) // [10, 20]
 }
